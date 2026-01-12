414d3f49400cc202fa0be0bac766344269f72c59
Edosgames <edomanini00@gmail.com>
2026-01-12 10:43:01 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,16 @@
-self.addEventListener("install", event => {
-    event.waitUntil(
-        caches.open("chess-cache").then(cache => {
-            return cache.addAll([
-                "/",
-                "/index.html",
-                "/style.css",
-                "/app.js",
-                "/manifest.json"
-            ]);
-        })
-    );
+// Service worker DISATTIVATO completamente
+
+// Questo file serve solo a eliminare ogni cache esistente
+// e disattivare qualsiasi service worker precedente.
+
+self.addEventListener("install", () => {
+    self.skipWaiting();
 });
 
-self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(keys.map(k => caches.delete(k)));
         })
     );
 });
